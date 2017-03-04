b7f6cde0463764c15fd988e3d1c977f61d263f92
Oliver Lorenz <lorenz@cybus.io>
2017-03-04 14:22:22 +0100

    save

--- code.ino
+++ code.ino
@@ -2,28 +2,167 @@
 #include <ESP8266WiFi.h>
 
 ///////////////////////////////////////////////////////////////////////////
-// Wifi Parameters
+// Network Parameters
 ///////////////////////////////////////////////////////////////////////////
-const char* ssid = "OFFLINE";
-const char* password = "kr0k0d!L3";
+const char* ssid = "<< WIFI SSID >>>";
+const char* password = "<< WIFI PASSWORD >>";
+uint8_t mac[6];
+
+WiFiClient wifiClient;
 
 ///////////////////////////////////////////////////////////////////////////
 // MQTT Parameters
 ///////////////////////////////////////////////////////////////////////////
-const char* mqttServer = "test.mosquitto.org";
-const char* mqttUsername = "your_username";
-const char* mqttPassword = "your_password";
-const int mqttPort = 1883;
+const char* mqttServer    = "192.168.0.253";
+const int   mqttPort      = 1883;
+const char* hitTopic      = "target/1/hit";
+const char* configTopic   = "target/1/config";
+const char* clientName    = "target1";
 
-const char* hitTopic;
-uint8_t mac[6];
-  
-void callbackMessageReceived(char* topic, byte* payload, unsigned int length) {
-  String payloadString = String(topic);
+long value = 0;
+long maxValue = 0;
+long threshold = 8;
+long average = 0;
+
+long loopCounter = 0;
+long maxLoops = 10;
+long totalValue = 0;
+bool hit = false;
+
+void blink() {
+  digitalWrite(2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(50);              // wait for a second
+  digitalWrite(2, LOW);    // turn the LED off by making the voltage LOW
+  delay(50);              // wait for a second
 }
 
-WiFiClient wifiClient;
-PubSubClient mqttClient(mqttServer, 1883, callbackMessageReceived, wifiClient);
+
+void callbackMessageReceived(char* topic, byte* payload, unsigned int length) {
+
+  // maxLoops threshold
+  for (int i=0; i<10; i++){
+    blink();
+  }
+  long newMaxLoops = int(payload[0]);
+  long newThreshold = int(payload[1]);
+
+  Serial.print("new config - maxLoops: ");
+  Serial.print(newMaxLoops);
+  Serial.print(", Threshold: ");
+  Serial.println(newThreshold);
+
+  maxLoops = newMaxLoops;
+  threshold = newThreshold;
+}
+
+PubSubClient mqttClient;
+
+
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+
+  pinMode(15, OUTPUT);
+  digitalWrite(15, 1);
+
+  initWifi();
+  initMQTT();
+}
+
+void resubscribe()
+{
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+
+  mqttClient.subscribe(configTopic);
+  Serial.println("subscribed to " + String(configTopic));
+}
+
+///////////////////////////////////////////////////////////////////////////
+// Init Wifi
+///////////////////////////////////////////////////////////////////////////
+void initWifi() {
+  Serial.print("wifi init. SSID: ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  WiFi.macAddress(mac);
+}
+
+///////////////////////////////////////////////////////////////////////////
+// Init MQTT
+///////////////////////////////////////////////////////////////////////////
+void initMQTT() {
+  mqttClient.setClient(wifiClient);
+  mqttClient.setServer(mqttServer, mqttPort);
+  mqttClient.setCallback(callbackMessageReceived);
+  mqttReconnect();
+}
+
+///////////////////////////////////////////////////////////////////////////
+// MQTT reconnect
+///////////////////////////////////////////////////////////////////////////
+void mqttReconnect() {
+  Serial.print("Connecting to MQTT-Server ");
+  Serial.print(mqttServer);
+  Serial.print(":");
+  Serial.print(mqttPort);
+  Serial.print(" with ClientId: ");
+  Serial.println(clientName);
+
+  // Loop until we're connected
+  while (!mqttClient.connected()) {
+    Serial.print(".");
+    if (mqttClient.connect(clientName)) {
+      Serial.println("");
+      Serial.println("connecting to MQTT server was successful!");
+      resubscribe();
+    }
+    delay(500);
+  }
+}
+
+void loop() {
+  value = analogRead(A0);
+
+  totalValue += value;
+
+  if (loopCounter > maxLoops) {
+    average = totalValue / maxLoops;
+    totalValue = 0;
+    loopCounter = 0;
+  }
+
+  if (average > threshold) {
+    hit = true;
+  } else {
+    if (hit == true) {
+      hit = false;
+      mqttClient.publish(hitTopic,"hit");
+      blink();
+      delay(150);
+    }
+  }
+  if (!mqttClient.connected()) {
+    mqttReconnect();
+  }
+
+  mqttClient.loop();
+  loopCounter++;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
 
 String macToStr(const uint8_t* mac)
 {
@@ -34,30 +173,6 @@ String macToStr(const uint8_t* mac)
       result += ':';
   }
   return result;
-}
-
-String getTopicBase(const uint8_t* mac) {
-  String topicName = "ESP8266/";
-  for (int i = 0; i < 6; ++i) {
-    topicName += String(mac[i], 16);
-    if (i < 5)
-      topicName += '-';
-  }
-  return topicName;
-}
-
-String getConfigurationTopic(const uint8_t* mac)
-{
-  String topicName = getTopicBase(mac);
-  topicName += "/config";
-  return topicName;
-}
-
-String getHitTopic(const uint8_t* mac)
-{
-   String topicName = getTopicBase(mac);
-  topicName += "/hit";
-  return topicName;
 }
 
 String getClientName(const uint8_t* mac)
@@ -71,105 +186,3 @@ String getClientName(const uint8_t* mac)
 
   return clientName;
 }
-
-void resubscribe()
-{
-  Serial.println("try to subscribe");
-  uint8_t mac[6];
-  WiFi.macAddress(mac);
-  
-  String subscriptionTopicString = getConfigurationTopic(mac);
-  int subscriptionTopicLength = subscriptionTopicString.length() + 1; 
-  
-  char subscriptionTopicCharArray[subscriptionTopicLength];
-  subscriptionTopicString.toCharArray(subscriptionTopicCharArray, subscriptionTopicLength);
-
-  if (mqttClient.subscribe(subscriptionTopicCharArray)) {
-    Serial.print("Subscribed to ");
-    Serial.println(subscriptionTopicString);
-  }
-}
-
-void setup() {
-  Serial.begin(115200);
-  delay(10);
-
-  Serial.println();
-  Serial.println();
-
-  ///////////////////////////////////////////////////////////////////////////
-  // Wifi Setup
-  ///////////////////////////////////////////////////////////////////////////
-  
-  Serial.print("Connecting to SSID: ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  WiFi.macAddress(mac);
-
-  ///////////////////////////////////////////////////////////////////////////
-  // MQTT setup
-  ///////////////////////////////////////////////////////////////////////////
-  
-  String clientName;
-  clientName = getClientName(mac);
-
-  Serial.print("Connecting to MQTT-Server ");
-  Serial.print(mqttServer);
-  Serial.print(" with ClientId: ");
-  Serial.println(clientName);
-
-  mqttReconnect(true);
-  hitTopic = getHitTopic(mac).c_str();
-}
-
-void mqttReconnect(bool abortIfFailed) {
-  String clientName;
-  clientName = getClientName(mac);
-  
-  // Loop until we're reconnected
-  while (!mqttClient.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    if (mqttClient.connect((char*) clientName.c_str(), mqttUsername, mqttPassword)) {
-      Serial.println("connected. resubscribing");
-      resubscribe();
-      Serial.println("done");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.println(mqttClient.state());
-      if (abortIfFailed) {
-        Serial.println("Will reset and try again...");
-        abort();
-      } else {
-        Serial.println(" try again in 5 seconds");
-        // Wait 5 seconds before retrying
-        delay(5000);
-      }
-      
-    }
-  }
-}
-
-long value = 0;
-
-void loop() {
-  if (!mqttClient.connected()) {
-    delay(1000);
-    mqttReconnect(false);
-  }
-  value = analogRead(0);
-  mqttClient.loop();
-  mqttClient.publish(hitTopic, String(value).c_str(), true);
-}
-
-
